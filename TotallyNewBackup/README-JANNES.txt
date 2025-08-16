4891529dbae3fda9c874a56858acbf55ff8e16c7
Jannes123 <jannes.engelbrecht2@gmail.com>
2025-08-16 13:18:28 +0000

    Midnight changes

---
+++ TotallyNewBackup/README-JANNES.txt
@@ -0,0 +1,11 @@
+This is the live Vite React project.
+
+To run the dev server:
+cd ~/GlassReal/TotallyNewBackup
+npm run dev
+
+Public URL:
+http://154.65.97.4:3000/
+
+Deployed manually from Codespaces midnight-export branch.
+- Hugo
--- TotallyNewBackup/src/main.tsx
+++ TotallyNewBackup/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./landing"; // 👈 make sure this matches your actual file
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
